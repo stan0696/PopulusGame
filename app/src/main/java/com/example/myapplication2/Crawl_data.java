820df6712224ffa0d0dd547164985a17a2f1de54
stan0696 <39663415+stan0696@users.noreply.github.com>
2019-06-24 11:00:32 +0800

    jsoup

--- app/src/main/java/com/example/myapplication2/Crawl_data.java
+++ app/src/main/java/com/example/myapplication2/Crawl_data.java
@@ -1,13 +1,30 @@
 package com.example.myapplication2;
 
-import android.provider.DocumentsContract.Document;
-import org.jsoup.jsoup;
+
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
 
 public class Crawl_data {
     private  String url;
     public Crawl_data(String url){
         this.url=url;
     }
-    Document document = Jsoup
+
+    public void run(){
+        try {
+            Document document = Jsoup.connect("https://www.taptap.com").get();
+            String str = document.title();
+            Log.e("标题",str);
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
