53081035860b270a016f58dbdcc3e04fcc07e67d
stan0696 <39663415+stan0696@users.noreply.github.com>
2019-06-28 19:16:39 +0800

    6-28 updata

---
+++ app/src/main/java/com/example/myapplication2/FragmentDetails.java
@@ -0,0 +1,18 @@
+package com.example.myapplication2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.myapplication2.R;
+
+public class FragmentDetails extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_details, container, false);
+        return rootView;
+    }
+}
