31ef7bc45fde7aaf3b3bb586e577229b9ae5b8ff
Gongyi-season <1904737989@qq.com>
2019-06-28 10:38:17 +0800

    6/28 Gongyi

---
+++ app/src/main/java/com/example/myapplication2/View/FocusGameActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication2.View;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.myapplication2.R;
+
+public class FocusGameActivity extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.focus_game, container, false);
+        return rootView;
+    }
+}
