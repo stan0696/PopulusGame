7a63cb1bf4ae44638ed053b3c92cdaa17a4c1e30
stan0696 <39663415+stan0696@users.noreply.github.com>
2019-06-28 19:11:48 +0800

    6-28 updata

--- app/src/main/java/com/example/myapplication2/GameinfoActivity.java
+++ app/src/main/java/com/example/myapplication2/GameinfoActivity.java
@@ -1,31 +1,56 @@
 package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.myapplication2.Tools.DownloadActivity;
+import com.example.myapplication2.Tools.FindGame;
+import com.example.myapplication2.Tools.SearchActivity;
+import com.example.myapplication2.View.Game;
 import com.example.myapplication2.View.MyImageView;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.net.URI;
+import java.util.ArrayList;
 
 public class GameinfoActivity extends AppCompatActivity {
 
     private MyImageView iconview;
     private TextView introductiontext;
     private TextView nametext;
-    private TextView tag1text;
-    private TextView tag2text;
-    private TextView tag3text;
+    private Button tag1text;
+    private Button  tag2text;
+    private Button  tag3text;
+    private Button  button_focus;
+    private Button  button_download;
     private String name;
     private String iconurl;
+    private String downloadurl;
     private String introduction;
     private String tag1;
     private String tag2;
     private String tag3;
+    private TabLayout tabLayout;
+    private ViewPager mViewPager;
+    List<Fragment> fragmentList = new ArrayList<>();
+    Fragment fragmentDetails;
+    Fragment fragmentComment;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,8 +58,41 @@ public class GameinfoActivity extends AppCompatActivity {
         Intent intent = getIntent();
         name = intent.getStringExtra("gamename");
         getdata();
+        mViewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.mytab);
+        initFragments();
+        tabLayout.setupWithViewPager(mViewPager);
+        tabLayout.getTabAt(0).setText("详情");
+        tabLayout.getTabAt(1).setText("评论");
     }
 
+    private void initFragments(){
+        fragmentDetails = new FragmentDetails();
+        fragmentComment = new FragmentComment();
+        fragmentList.add(fragmentDetails);
+        fragmentList.add(fragmentComment);
+        mViewPager.setAdapter(new ViewPagerAdapter(fragmentList,getSupportFragmentManager()));
+        setlistener();
+    }
+
+    private void setlistener()
+    {
+        button_download=findViewById(R.id.button_download);
+
+        button_download.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GameinfoActivity.this.getBaseContext(), DownloadActivity.class);
+                /**此处需修改*/ intent.putExtra("downloadurl","http://mhhy.dl.gxpan.cn/apk/ml/MBGYD092101/Gardenscapes-ledou-MBGYD092101.apk");
+                startActivity(intent);
+
+
+
+            }
+
+
+        });
+    }
     private void getdata() {
         SQLiteDbHelper helper = new SQLiteDbHelper(getApplicationContext());
         SQLiteDatabase database = helper.getWritableDatabase();
@@ -47,8 +105,12 @@ public class GameinfoActivity extends AppCompatActivity {
             this.name= cursor.getString(0);
             this.iconurl=cursor.getString(2);
             this.introduction=cursor.getString(3);
+            this.downloadurl=cursor.getString(4);
             this.tag1=cursor.getString(6);
             this.tag2=cursor.getString(7);
+            this.tag3=cursor.getString(8);
+            System.out.println(iconurl);
+            System.out.println(123);
             iconview=(MyImageView)findViewById(R.id.imageView_game);
             introductiontext=findViewById(R.id.game_textIntroduce);
             nametext=findViewById(R.id.game_textname);
@@ -72,6 +134,9 @@ public class GameinfoActivity extends AppCompatActivity {
                 tag3text=findViewById(R.id.gametag3);
                 tag3text.setText(tag3);
             }
+
+
+
 
         }
     }
--- app/src/main/java/com/example/myapplication2/Tools/FindGame.java
+++ app/src/main/java/com/example/myapplication2/Tools/FindGame.java
@@ -1,15 +1,117 @@
 package com.example.myapplication2.Tools;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.UrlQuerySanitizer;
+
+import com.example.myapplication2.SQLiteDbHelper;
+import com.example.myapplication2.View.Game;
+
 import java.net.URI;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class FindGame {
     private  String tag;
     private URI url;
+    private Game game;
+    private ArrayList<Game>  games;
     public  FindGame(URI url,String tag){
         this.tag=tag;
         this.url=url;
+        games=new ArrayList<>();
     }
+    public Game findgamebyname(String name, Context context)
+    {
+        Game thegame=new Game(name);
+
+        SQLiteDbHelper helper = new SQLiteDbHelper(context);
+        SQLiteDatabase database = helper.getWritableDatabase();
+        ContentValues cValue = new ContentValues();
+        String [] names=new String[1];
+        names[0]=name;
+        Cursor cursor = database.query("game", null, "name=?", new String[]{name}, null, null, null);//修改
+        if (cursor.moveToFirst()) {
+
+            int id = Integer.parseInt( cursor.getString(1) );
+            thegame.setID(id);
+
+            thegame.setIcon(cursor.getString(2));
+
+            thegame.setIntroduction(cursor.getString(3));
+
+            //URI downloaduri=URI.create(cursor.getString(4));
+           // thegame.setDownloadUrl(downloaduri);
+
+            thegame.setTag(1,cursor.getString(6));
+            thegame.setTag(2,cursor.getString(7));
+            thegame.setTag(3,cursor.getString(8));
+
+            //int mark = Integer.parseInt( cursor.getString(9) );
+            //thegame.setMark( mark);
+
+
+
+
+        }
+        return thegame;
+    }
+    public ArrayList<Game> findgamebytag(String tag, Context context)
+    {
+
+        SQLiteDbHelper helper = new SQLiteDbHelper(context);
+        SQLiteDatabase database = helper.getWritableDatabase();
+       // ContentValues cValue = new ContentValues();
+
+        Cursor cursor1 = database.query("game", null, "tag1=? or tag2=? or tag3=?", new String[]{tag,tag,tag}, null, null, null);//修改
+        if (cursor1.moveToFirst())
+        {
+            while(cursor1.moveToNext())
+            {
+
+
+            Game thegame=new Game("name");
+
+            thegame.setName(cursor1.getString(0));
+
+            int id = Integer.parseInt( cursor1.getString(1) );
+            thegame.setID(id);
+
+            thegame.setIcon(cursor1.getString(2));
+
+            thegame.setIntroduction(cursor1.getString(3));
+
+                thegame.setTag(1,cursor1.getString(6));
+                thegame.setTag(2,cursor1.getString(7));
+                thegame.setTag(3,cursor1.getString(8));
+
+            //URI downloaduri=URI.create(cursor1.getString(4));
+            //thegame.setDownloadUrl(downloaduri);
+
+
+            //int mark = Integer.parseInt( cursor1.getString(9) );
+           // thegame.setMark( mark);
+            games.add(thegame);
+
+            }
+
+        }
+
+        return games;
+    }
+
+
+
+
+
+
+
+
+
+
+
     public String getTag() {
         return tag;
     }
