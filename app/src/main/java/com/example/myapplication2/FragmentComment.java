7ca6899ae9b3becb4d11b0a80eff1638e74c186f
Gongyi-season <1904737989@qq.com>
2019-06-28 17:01:39 +0800

    6/28 Gongyi

---
+++ app/src/main/java/com/example/myapplication2/FragmentComment.java
@@ -0,0 +1,18 @@
+package com.example.myapplication2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.myapplication2.R;
+
+public class FragmentComment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_comments, container, false);
+        return rootView;
+    }
+}
