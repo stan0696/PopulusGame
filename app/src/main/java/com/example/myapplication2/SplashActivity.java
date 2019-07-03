ea87f2c67eeedbb81c852454480f57d710c935c5
Gongyi-season <1904737989@qq.com>
2019-07-03 18:54:35 +0800

    Merge branch 'master' of https://github.com/stan0696/PopulusGame
    # Conflicts:
    #	app/src/main/java/com/example/myapplication2/GameinfoActivity.java

--- app/src/main/java/com/example/myapplication2/SplashActivity.java
+++ app/src/main/java/com/example/myapplication2/SplashActivity.java
@@ -2,8 +2,11 @@ package com.example.myapplication2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -14,6 +17,7 @@ public class SplashActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setStatusBarFullTransparent();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -22,5 +26,22 @@ public class SplashActivity extends Activity{
                 SplashActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGHT);
+    }
+    /**
+     * 全透状态栏
+     */
+    protected void setStatusBarFullTransparent() {
+        if (Build.VERSION.SDK_INT >= 21) {//21表示5.0
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= 19) {//19表示4.4
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //虚拟键盘也透明
+            //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
     }
 }
